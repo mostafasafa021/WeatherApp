c3ab8ba00bfea3abc36dad7dc4176df7a9ff2acb
Mostafa Safa <mostafasafaarchon021@gmail.com>
2026-09-18 14:02:32 +0300

    Make Search UI Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,13 @@ function App() {
         <h1 className="text-white text-6xl tracking-wider font-semibold page-title text-center my-15 font-bricolage-grotesque">
           How's the sky looking today?
         </h1>
-        <form className="flex flex-col gap-3">
-          <div className="input-wrapper relative">
+        <form className="flex flex-col gap-3 md:flex-row">
+          <div className="input-wrapper relative flex-1">
             <input
               type="text"
-              className="text-white bg-neutral-700 py-3 rounded-[10px] pl-15 pr-5 w-full placeholder:text-[#f1f1f4] placeholder:tracking-wide cursor-pointer focus:border-3 focus:border-neutral-900 focus:outline-white focus:outline-2 "
+              className="text-white  bg-neutral-700 py-4 rounded-[12px] pl-15 pr-5 w-full placeholder:text-[#f1f1f4] placeholder:tracking-wide cursor-pointer focus:border-3 focus:border-neutral-900 focus:outline-white focus:outline-2 "
               placeholder="Search for a place..."
+              aria-label="Search for a place"
             />
             <img
               src={searchIcon}
@@ -36,7 +37,7 @@ function App() {
               className="absolute left-8 top-2/4 -translate-2/4 "
             />
           </div>
-          <button className="text-white bg-blue-500 py-3 rounded-[10px]  focus:border-3 focus:border-neutral-900 focus:outline-blue-700 focus:outline-2 cursor-pointer hover:bg-blue-700 transition-colors">
+          <button className="text-white bg-blue-500 py-4 px-7 rounded-[12px]  focus:border-3 focus:border-neutral-900 focus:outline-blue-700 focus:outline-2 cursor-pointer hover:bg-blue-700 transition-colors">
             Search
           </button>
         </form>
