9a367ee71eb1086a9f8e4caff59551d202d7fbf0
Mostafa Safa <mostafasafaarchon021@gmail.com>
2026-09-18 13:35:35 +0300

    Added Search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./components/Header";
+import searchIcon from "./assets/images/icon-search.svg";
 
 function App() {
-  return <div>Hello World</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [countriesData, setCountriesData] = useState(null);
+
+  // useEffect(() => {
+  //   if (searchTerm === "" || searchTerm.length < 2) return;
+  //   axios
+  //     .get(
+  //       `https://geocoding-api.open-meteo.com/v1/search?name=${searchTerm}&count=4`,
+  //     )
+  //     .then((response) => setCountriesData(response.data.results));
+  // }, [searchTerm]);
+  return (
+    <div className="app bg-neutral-900">
+      <main className="pt-10 px-5 md:px-10">
+        <Header />
+        <h1 className="text-white text-6xl tracking-wider font-semibold page-title text-center my-15 font-bricolage-grotesque">
+          How's the sky looking today?
+        </h1>
+        <form className="flex flex-col gap-3">
+          <div className="input-wrapper relative">
+            <input
+              type="text"
+              className="text-white bg-neutral-700 py-3 rounded-[10px] pl-15 pr-5 w-full placeholder:text-[#f1f1f4] placeholder:tracking-wide cursor-pointer focus:border-3 focus:border-neutral-900 focus:outline-white focus:outline-2 "
+              placeholder="Search for a place..."
+            />
+            <img
+              src={searchIcon}
+              alt=""
+              className="absolute left-8 top-2/4 -translate-2/4 "
+            />
+          </div>
+          <button className="text-white bg-blue-500 py-3 rounded-[10px]  focus:border-3 focus:border-neutral-900 focus:outline-blue-700 focus:outline-2 cursor-pointer hover:bg-blue-700 transition-colors">
+            Search
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SettingButton.jsx
@@ -0,0 +1,17 @@
+import unitsIcon from "../assets/images/icon-units.svg";
+import dropdownIcon from "../assets/images/icon-dropdown.svg";
+
+const SettingButton = () => {
+  return (
+    <div className="setting-btn-wrapper relative">
+      <button className="text-white flex gap-2.5 bg-neutral-700 py-3 px-4.5 rounded-[6px] cursor-pointer setting-btn">
+        <img src={unitsIcon} alt="" />
+        Units
+        <img src={dropdownIcon} alt="" />
+      </button>
+      <ul className="dropdownList absolute "></ul>
+    </div>
+  );
+};
+
+export default SettingButton;
