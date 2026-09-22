6a55e515245b65106344e2b6cddec5d60ddd08fc
Mostafa Safa <mostafasafaarchon021@gmail.com>
2026-09-22 18:08:37 +0300

    Added loading State to searchFeature and no result State

---
+++ src/components/DisplaySearchResults.jsx
@@ -0,0 +1,43 @@
+import loadingIcon from "../assets/images/icon-loading.svg";
+
+const DisplaySearchResults = ({
+  searchResults,
+  handleClick,
+  searchResultsLoading,
+  isNoResults
+}) => {
+  if (searchResultsLoading) {
+    return (
+      <div className="w-full absolute top-[130%] rounded-[10px] border-2 flex gap-4 items-center border-neutral-600 py-3 px-2 text-neutral-0">
+        <img src={loadingIcon} alt="" className="loading" />
+        <p className="text-[1.2rem]">Search in progress</p>
+      </div>
+    );
+  }
+
+  if (isNoResults) {
+    return <div className="w-full absolute top-[170%] text-neutral-0 text-[1.5rem] font-medium text-center tracking-wide">No Search Results Found!</div>
+  }
+
+  return (
+    <>
+      {searchResults.length > 0 && (
+        <div className="w-full absolute bg-neutral-800 top-[130%] rounded-[10px] border-2 border-neutral-600">
+          {
+            <ul className="py-3 px-4">
+              {searchResults.map((country) => (
+                <button
+                  className="text-neutral-0 py-3 px-2 text-[1.2rem] focus:outline-0 focus:bg-neutral-700 focus:border-neutral-600 focus:border-2 rounded-[10px] block w-full text-start"
+                  key={country.id}
+                  onClick={() => handleClick(country.id)}
+                >{`${country.name}, ${country.admin1 ?? ""}, ${country.country}`}</button>
+              ))}
+            </ul>
+          }
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DisplaySearchResults;
---
+++ src/components/SearchField.jsx
@@ -0,0 +1,38 @@
+import searchIcon from "../assets/images/icon-search.svg";
+import DisplaySearchResults from "./DisplaySearchResults";
+
+const SearchField = ({
+  setSearchTerm,
+  searchTerm,
+  searchResults,
+  handleClick,
+  handleSubmit,
+  searchResultsLoading,
+  isNoResults,
+}) => {
+  return (
+    <form className="flex flex-col gap-3 md:flex-row" onSubmit={handleSubmit}>
+      <div className="input-wrapper relative flex-1">
+        <input
+          type="text"
+          className="text-white  bg-neutral-700 py-4 rounded-[12px] pl-15 pr-5 w-full placeholder:text-[#f1f1f4] placeholder:tracking-wide cursor-pointer focus:border-3 focus:border-neutral-900 focus:outline-white focus:outline-2 "
+          placeholder="Search for a place..."
+          aria-label="Search for a place"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={searchIcon}
+          alt=""
+          className="absolute left-8 top-2/4 -translate-2/4"
+        />
+        <DisplaySearchResults searchResults={searchResults} handleClick={handleClick} searchResultsLoading={searchResultsLoading} isNoResults={isNoResults}/>
+      </div>
+      <button className="text-white bg-blue-500 py-4 px-7 rounded-[12px]  focus:border-3 focus:border-neutral-900 focus:outline-blue-700 focus:outline-2 cursor-pointer hover:bg-blue-700 transition-colors">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchField;
